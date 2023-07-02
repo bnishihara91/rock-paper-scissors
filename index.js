e9bfb092615849cd2cbd0376dc4f9b4b2a72521e
Bruce Nishihara <bnishihara@yahoo.com>
2023-07-02 08:00:33 -0700

    Add return to random computer choice

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const options = ["rock", "paper", "scissor"];
 
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
 }
+
+getComputerChoice();
