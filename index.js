0ade2845d4a9e37b859ec012368d2b48fddf5363
Bruce Nishihara <bnishihara@yahoo.com>
2023-07-02 08:13:27 -0700

    Edit checkWinner function

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ function getComputerChoice() {
 }
 
 function checkWinner(playerSelection, computerSelection) { 
-    
+    if (playerSelection == computerSelection) { 
+        return "It's a tie.";
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+        ){
+            return "Player Wins!";
+        } else {
+            return "Computer Wins."
+        }
+
 }
 
 function playRound(playerSelection, computerSelection) {
